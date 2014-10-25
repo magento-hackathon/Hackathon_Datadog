bd52bb033dc9bd1c58bc99c435ff4b1f68b0ee1d
Jacques Bodin-Hullin <j.bodinhullin@monsieurbiz.com>
2014-10-25 13:08:25 +0200

    [+FEATURE] Create basics: config, models (empty) and helper (empty)

---
+++ app/code/community/Hackathon/Metrics/Model/Config.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This file is part of Hackathon_Metrics for Magento.
+ *
+ * @license OSL v3
+ * @author Jacques Bodin-Hullin <j.bodinhullin@monsieurbiz.com> <@jacquesbh>
+ * @category Hackathon
+ * @package Hackathon_Metrics
+ * @copyright Copyright (c) 2014 Magento Hackathon (http://mage-hackathon.de)
+ */
+
+/**
+ * Config Model
+ * @package Hackathon_Metrics
+ */
+class Hackathon_Metrics_Model_Config extends Mage_Core_Model_Abstract
+{
+
+}
